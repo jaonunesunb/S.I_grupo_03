4b9fd37a642f26c33ead42922751c8cc8ba84571
Mateus Santos <santos.mateus@protonmail.com>
2023-09-26 19:04:58 -0300

    only get events greater than current date

--- src/services/events/getAllEvents.service.ts
+++ src/services/events/getAllEvents.service.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getAllEventsService = async () => {
+  const dataAtual = new Date(new Date().toDateString());
+  
   const events = await prisma.evento.findMany({
+    where: { dataFim: { gte: dataAtual } },
     include: {
       criador: true,
       departamento: true,
